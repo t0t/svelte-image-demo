e4a8ed166e7afc6ca9f33afcc07077f559617117
Sergio Forés <sergiofores@gmail.com>
2020-05-30 12:04:54 +0200

    layout css grid

--- src/pages/blog/[slug].svelte
+++ src/pages/blog/[slug].svelte
@@ -1,29 +1,57 @@
 <script>
     // import Image from "svelte-image";
+    // import { onMount } from 'svelte';
     import { url } from "@sveltech/routify";
     export let slug;
     import data from "./blog.json";
     const posts = data.posts;
+    let content = data.posts.content;
+    console.log(posts);
+    console.log(slug);
+    console.log(content);
+
+	// let photos = [];
+
+	// onMount(async () => {
+	// 	const res = await fetch(`./blog.json`);
+	// 	photos = await res.json();
+	// });
+    // console.log(photos);
 </script>
 
 <style lang="scss">
     @import "../../styles/main.scss";
+    .PostArticle .p {
+        color: pink;
+    }
     .Post {
         padding: $h3;
-        display: grid; 
-        grid-template-columns: repeat(2, 1fr); 
+        /* display: grid; 
+        grid-template-columns: repeat(2, 1fr);  */
         /* grid-auto-rows: 100px;  */
 
         &Article {
-            grid-column-start: 1; 
-            grid-column-end: 4; 
-            grid-row-start: 1; 
-            grid-row-end: 3;
+            h3 {
+                text-align: center;
+            }
         }
         &Content {
-            grid-column-start: 1; 
-            grid-row-start: 3; 
-            grid-row-end: 5; 
+            display: grid;
+            /* grid-auto-flow: column; */
+            @include media(s2) {
+                grid-template-columns: repeat(auto-fill, minmax($h8, 1fr));
+                border: 1px solid $light_grey;
+                padding: $h3;
+            }
+            @include media(s3) {
+                grid-template-columns: minmax($h6, $h8) 1fr;
+                /* grid-template-columns: repeat(auto-fill, minmax($h9, 1fr)); */
+                /* justify-items: center; */
+            }
+            @include media(s4) {
+                grid-template-columns: repeat(auto-fill, minmax($h10, 1fr));
+            }
+            gap: $h3;
         }
         &Header {
             text-align: center;
@@ -45,14 +73,30 @@
                     {post.title}
                 </h1>
                 <h3>
-                    {@html post.excerpt}
+                    {@html post.subtitle}
                 </h3>
                 <div class="PostImgContainer">
                     <img src="/{post.imagen}" alt="{post.title}"/>
                 </div>
-                <p class="PostContent">
-                    {@html post.content}
-                </p>
+                <div class="PostContent">
+                    <h4>{@html post.content.h1}</h4>
+                    <p>{@html post.content.p}</p>
+                    <blockquote>{@html post.content.blockquote}</blockquote>
+                    <p>{@html post.content.p2}</p>
+                    <p>{@html post.content.p3}</p>
+                    <p>{@html post.content.p4}</p>
+                    <p>{@html post.content.p5}</p>
+                    <p>{@html post.content.p6}</p>
+                    {@html post.content.img1}
+                    <p>{@html post.content.p7}</p>
+                    <p>{@html post.content.p8}</p>
+                    <p>{@html post.content.p9}</p>
+                    <p>{@html post.content.p10}</p>
+                    {@html post.content.img2}
+                    <p>{@html post.content.p11}</p>
+                    <p>{@html post.content.p12}</p>
+                    <p>{@html post.content.p13}</p>
+                </div>
             </article>
         {/if}
     {/each}
